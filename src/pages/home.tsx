9f954e2114eef7a5d510dc22066e7543a027f335
abishek-trainn <abishek@trainn.co>
2023-03-31 20:10:58 +0530

    wip: changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,16 +8,6 @@ export default function Home() {
       </Head>
       <div className="container mx-auto min-h-screen w-full max-w-[70rem]  p-3 sm:p-4">
         <h1 className="text-3xl font-semibold">Welcome to snapx.</h1>
-        <iframe
-          width="560"
-          height="315"
-          src="http://localhost:3000/embed/d39JWuwKAB"
-          title="Research / Trends in ai - 19 March 2023"
-          allow="accelerometer; autoplay; clipboard-write;"
-          frameBorder="0"
-          className="rounded-xl"
-          allowFullScreen={true}
-        ></iframe>
       </div>
     </>
   );
